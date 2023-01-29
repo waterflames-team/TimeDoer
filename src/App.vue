d4c21903bbafb8206b45fd14e2517425c152511a
Kaichen <276126767@qq.com>
2023-01-29 16:16:28 +0100

    feat: 主题切换

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script lang='ts' setup>
 import { onMounted, ref } from 'vue';
 import { initStore, initSQL, getStore } from '@/utils/index';
-
-const theme = ref('light')
+import { listen } from '@tauri-apps/api/event';
+const theme = ref()
 
 onMounted(async () => {
   initStore();
   initSQL()
-  theme.value = await getStore('theme')
+  theme.value = await getStore('theme') || 'light';
+
+  // 添加主题更改监听
+  await listen('changeTheme', ({ payload }) => {
+    theme.value = payload
+  })
 });
 </script>
 
@@ -23,8 +28,9 @@ onMounted(async () => {
 .content {
   background: var(--background);
   color: var(--text);
+  font-family: misan-bold;
 
-  @apply flex justify-center items-center h-screen overflow-hidden break-all
+  @apply flex justify-center items-center h-screen overflow-hidden break-all relative
 }
 </style>
 
