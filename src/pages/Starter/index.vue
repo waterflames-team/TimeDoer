d90f52713fd777023d5932ac5e004d49a397a3d2
Kaichen <276126767@qq.com>
2023-01-29 03:34:46 +0100

    feat: 起始页

---
+++ src/pages/Starter/index.vue
@@ -0,0 +1,54 @@
+<script lang='ts' setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const titleShow = ref(false)
+
+setTimeout(() => {
+  titleShow.value = true
+}, 1000)
+
+setTimeout(() => {
+  titleShow.value = false
+  setTimeout(() => {
+    router.push('/main')
+  }, 1000)
+}, 3000)
+</script>
+
+<template>
+  <Transition name="bounce">
+    <div class="flex flex-col items-center gap-4" v-show="titleShow">
+      <div class="flex gap-4">
+        <span class="font-[alimama] text-7xl">睹时</span>
+        <span class="font-[alimama] text-7xl">TimeDoer</span>
+      </div>
+      <div class="font-[alimama] text-4xl">用时钟带领一天</div>
+    </div>
+  </Transition>
+</template>
+
+<style lang="scss" scoped>
+.bounce-enter-active {
+  animation: bounce-in 1.0s;
+}
+
+.bounce-leave-active {
+  animation: bounce-in 1.0s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+
+  50% {
+    transform: scale(1.15);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
